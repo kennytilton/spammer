1df19cc97e10d589541ca0877bc8de9a5d634314
Kenneth Tilton <kentilton@gmail.com>
2018-03-27 16:43:43 -0400

    pick up some files

---
+++ src/spamux/batcher.clj
@@ -0,0 +1,318 @@
+(ns spamux.batcher
+  (:require
+    [config.core :refer [env]]
+    [clojure.string :as str]
+    [clojure.pprint :as pp]
+    [clojure.edn :as edn]
+    [clojure.core.async :refer [go chan go-loop <! <!! >!! >!
+                                timeout alt!!]]
+    [clojure.tools.cli :refer [parse-opts] :as cli]
+    [taoensso.tufte :as tufte :refer (defnp p profiled profile)]
+    [taoensso.timbre :as log]
+    [spamux.genlist :refer :all]))
+
+(def spamgen-cli
+  [["-t" "--test TESTFILE" "Just filename and extension of a file under bulkinput/"
+    :id :test-input-file
+    :default "em-100000-100.edn"
+    ;; todo check here that file exists
+    ;; :validate [#(<= 1 %) "Must be a number greater than one (1)"]
+    ]
+   ["-v" "--verbose"]
+
+   ["-h" "--help"]])
+
+(def env-hack
+  ;; todo persuade lein bin and config to work together
+  {
+   :smtp              ["1.2.3.4" "10.20.30.40" "100.200.101.201"
+                       "11.22.33.44" "111.112.113.114" "22.33.44.55"]
+   :worker-ct         6
+   :individual-max    0.3
+   :overall-mean-max  0.05
+   :last-n-mean-max   0.1
+   :last-n-span       100
+   :bulkmail-out-path "bulkmail"
+   })
+
+(declare pln now xpln email-file-to-sendfiles-mp)
+
+#_(-main "-v")
+
+(defn -main [& args]
+  ;; uncomment during development so errors get through when async in play
+  #_(Thread/setDefaultUncaughtExceptionHandler
+      (reify Thread$UncaughtExceptionHandler
+        (uncaughtException [_ thread ex]
+          (log/error {:what      :uncaught-exception
+                      :exception ex
+                      :where     (str "Uncaught exception on" (.getName thread))}))))
+
+  (let [input (cli/parse-opts args spamgen-cli)
+        {:keys [options arguments summary errors]} input
+        {:keys [verbose test-input-file help]} options]
+
+    (cond
+      errors (doseq [e errors]
+               (println e))
+
+      help (println "\nUsage:\n\n    spamgen <input-edn> options*\n\n"
+             "Options:\n" (subs summary 1))
+
+      :default (email-file-to-sendfiles-mp
+                 (str "bulkinput/" test-input-file)
+                 verbose)))
+
+  ;; WARNING: comment this out for use with REPL. Necessary, to
+  ;; get standalone version to exit reliably.
+  ;;
+  ;;(shutdown-agents)
+  )
+
+(defn run-batch [test-input-file]
+  (email-file-to-sendfiles-mp
+    (str "bulkinput/" test-input-file)
+    false))
+
+(declare
+  emw-email-consider
+  span-mean-ok
+  edn-dump pln)
+
+(def batch-abort? (atom nil))
+
+(defn batch-abort []
+  (reset! batch-abort? true))
+
+(defn email-file-to-sendfiles-mp
+  "[em-file (coll)]
+
+  Produce one or more output files targeted
+  for different SMTP servers constraining the sequence of emails
+  in each to honor spam score constraints specified in config.edn
+  and never to include two emails to the same address across all
+  batches."
+  [em-file verbose]
+
+  (reset! batch-abort? false)
+
+  (let [em-addrs-hit (ref #{})
+        shared-chan (chan)
+        spit-chan (chan)
+
+        workers (map (fn [id smtp-ip]
+                       {:id        id
+                        :smtp-ip   smtp-ip
+                        :ch        shared-chan
+                        :spit-chan spit-chan
+                        :out-buff  (atom nil)
+                        :addrs-hit em-addrs-hit
+                        :spit-file (str
+                                     (:bulkmail-out-path env-hack) "/em-"
+                                     smtp-ip ".edn")
+
+                        :stats     (atom {:start-time            (System/currentTimeMillis)
+                                          :sent-ct               0
+                                          :last-n-mean           0
+                                          :spam-score-sum        0
+                                          :rejected-score        0
+                                          :rejected-dup-addr     0
+                                          :rejected-overall-mean 0
+                                          :rejected-span-mean    0})})
+                  (range)
+                  (take (min (count (:smtp env-hack))
+                          (:worker-ct env-hack))
+                    (:smtp env-hack)))
+
+        work-procs (doall
+                     (map (fn [w]
+                            (go-loop []
+                              (when-let [em-chunk (<! (:ch w))]
+                                ;;(pln :got-chunk (count em-chunk))
+                                (doseq [em em-chunk]
+                                  (emw-email-consider w em))
+                                (recur))))
+                       workers))]
+
+    ;; --- initialize spit files with a header ----------------------
+
+    (doseq [w workers]
+      (spit (:spit-file w)
+        {:run-date (.toString (java.util.Date.))
+         :smtp-ip  (:smtp-ip w)}))
+
+    ;; --- start the spitter ---------------------------------------
+
+    (let [spitters
+          nil #_(doall (for [_ (range 5)]
+                         (p :spitting
+                           (go-loop []
+                             (if-let [x (<! spit-chan)]
+                               (let [[spit-file em] x]
+                                 (xpln :sptting spit-file em)
+                                 (spit spit-file em :append true)
+                                 (recur))
+                               (pln :spitter-out!!!!))))))]
+
+      ;; --- feed the workers ----------------------------------------
+
+      (pln :feeding!!!!)
+
+      (let [wait (atom 0)]
+        (p :feed-workers
+          (with-open [in (java.io.PushbackReader. (clojure.java.io/reader em-file))]
+            (let [edn-seq (repeatedly (partial edn/read {:eof :fini} in))]
+              (doseq [em-chunk (take-while (partial not= :fini) edn-seq)]
+                ;; todo switch to loop so we can break out
+                (if @batch-abort?
+                  (xpln :aborted!!!!!!!!!!!!!!!!)
+                  (let [start (System/currentTimeMillis)]
+                    (>!! shared-chan em-chunk)
+                    (swap! wait + (- (System/currentTimeMillis) start))))))))
+        (pln :feeder-waited @wait :miilis))
+
+      ;; --- let the workers finish ----------------------------------
+
+      (pln :waiting-on-workers)
+
+      (loop [[work-proc & rest :as ps] work-procs]
+        (when work-proc
+          (when-let [out (alt!!
+                           (timeout 100) :timeout
+                           work-proc ([r] r))]
+            ;;(pln :bam-worker-out work-proc)
+            (recur rest))))
+
+      (pln :waitingonspitterc)
+      (doseq [spitter spitters]
+        (when-let [out (alt!!
+                         (timeout 1000) :timeout
+                         spitter ([r] r))]
+          (print :spitwait out)))
+
+      ;; --- dump worker stats ---------------------------------------
+
+      (when verbose
+        (doseq [w workers]
+          (pln)
+          (pln (format "worker %d:" (:id w)))
+          (pp/pprint @(:stats w))
+          (pln)))
+
+      ;; ---- dump summary stats -------------------------------------
+
+      (let [start (apply min (map #(:start-time @(:stats %))
+                               workers))]
+        (pp/pprint (merge
+                     {:run-duration (format "%.2f seconds"
+                                      (/ (- (now) start) 1000.0))}
+                     (apply merge-with +
+                          (map #(select-keys @(:stats %)
+                                  [:sent-ct :rejected-score :rejected-dup-addr
+                                   :rejected-overall-mean
+                                   :rejected-span-mean])
+                            workers))))
+
+
+        (pln)
+        (println :fini)))))
+
+(defnp emw-email-consider
+  "[w (writer) task (email info)]
+  Decide whether this writer should include this email in
+  the batch, given its spam score and how it will affect the
+  overall spam score of this writer's output and the running
+  mean spam score of the most recent emails.
+
+  Simply write to the writer's batch or ignore. Output is meaningless."
+
+  [w task]
+
+  (xpln :consider (:spam-score task) (:email-address task))
+
+  (cond
+    (> (:spam-score task) (:individual-max env-hack))
+    (do
+      (swap! (:stats w) update-in [:rejected-score] inc)
+      (xpln :email-indy-bad :w (:id w) :score (:spam-score task)))
+
+    :default
+    (let [stats @(:stats w)
+          new-sum (+ (:spam-score task) (:spam-score-sum stats))
+          new-ct (inc (:sent-ct stats))]
+      (cond
+        (> (/ new-sum new-ct)
+          (:overall-mean-max env-hack))
+        (do (swap! (:stats w) update-in [:rejected-overall-mean] inc)
+            (xpln :overall-email-mean-bad :score (:spam-score task)
+              :new-mean (/ new-sum new-ct)
+              :limit (:overall-mean-max env-hack)))
+
+        (not (span-mean-ok w (:spam-score task)))
+        ;; todo save to "try later" array to be possibly
+        ;; incorporated later when running mean might drop
+        (do (swap! (:stats w) update-in [:rejected-span-mean] inc)
+            (pln :span-mean-bad (:spam-score task)))
+
+        :default
+        (if (get @(:addrs-hit w) (:email-address task))
+          (swap! (:stats w) update-in [:rejected-dup-addr] inc)
+          (do
+            (dosync (commute (:addrs-hit w) conj (:email-address task)))
+            (swap! (:stats w) merge {:sent-ct        new-ct
+                                     :spam-score-sum new-sum})
+
+            (xpln :sending-to (:spit-chan w) [(:spit-file w) task])
+
+            (when (>= (count @(:out-buff w)) 500)
+              (spit (:spit-file w) @(:out-buff w) :append true)
+              (reset! (:out-buff w) nil))
+
+            (swap! (:out-buff w) conj task)
+
+            #_(>!! (:spit-chan w)
+                [(:spit-file w) task])))))))
+
+(defnp span-mean-ok
+  "[w (writer) new-score (score of email being considered)]
+  Decide if this new score, if included, will violate running mean score
+  invariants specified in config.edn"
+
+  [w new-score]
+
+  (let [stats @(:stats w)
+        last-n-mean (:last-n-mean stats)
+        new-ct (min
+                 (:last-n-span env-hack)
+                 (inc (:sent-ct stats)))
+        new-mean (+ last-n-mean
+                   (/ (- new-score last-n-mean) new-ct))]
+    (if (<= new-mean (:last-n-mean-max env-hack))
+      (do
+        (swap! (:stats w) assoc :last-n-mean new-mean)
+        #_(pln :okspan new-ct new-mean new-score (:last-n-mean-max env-hack))
+        true)
+      (do
+        #_(pln :failspan new-ct new-mean (:last-n-mean-max env-hack))
+        false))))
+
+;;; --- utilities -------------------------------------------------
+
+(defn edn-dump                                              ;; todo turn into validator
+  ([path]
+   (edn-dump path (str "edn-dumping " path)))
+
+  ([path banner]
+   (println banner)
+   (with-open [in (java.io.PushbackReader. (clojure.java.io/reader path))]
+     (let [edn-seq (repeatedly (partial edn/read {:eof :fini} in))]
+       (dorun (map println (take-while (partial not= :fini) edn-seq)))))))
+
+(defn pln [& args]
+  (locking *out*
+    (println (str/join " " args))))
+
+(defn xpln [& args])
+
+(defn now []
+  (System/currentTimeMillis))
---
+++ src/spamux/genlist.clj
@@ -0,0 +1,82 @@
+(ns spamux.genlist
+  (:require
+    [clojure.spec.alpha :as s]
+    [clojure.spec.gen.alpha :as gen]
+    ))
+
+(def email-domains
+  #{"indeediot.com"
+    "monstrous.com"
+    "linkedarkpattern.com"
+    "dired.com"
+    "lice.com"
+    "careershiller.com"
+    "glassbore.com"})
+
+(def email-regex
+  #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
+
+(s/def ::email-address
+  (s/with-gen
+    (s/and string? #(re-matches email-regex %))
+    #(->>
+       (gen/tuple (gen/such-that not-empty (gen/string-alphanumeric))
+         (s/gen email-domains))
+       (gen/fmap (fn [[addr domain]] (str addr "@" domain))))))
+
+(s/def ::email-body
+  (s/with-gen
+    (s/and string?)
+    #(->>
+       (gen/such-that not-empty (gen/string-alphanumeric))
+       (gen/fmap (fn [msg] (str "Hi! Buy " msg " at half off!"))))))
+
+(s/def ::spam-score
+  (s/double-in :min 0.0 :max 1))
+
+(s/def ::email-record
+  (s/keys :req-un [::email-address ::spam-score]))
+
+(defn email-records-test-gen
+  ([] (repeatedly
+        #(gen/generate (s/gen ::email-record))))
+  ([n]
+   (if (nil? n)
+     (email-records-test-gen)
+     (take n (email-records-test-gen)))))
+
+#_
+(let [chunk 100
+      total 5000000]
+  (let [chunks (partition-all chunk
+                 (email-records-test-gen total))]
+    (dorun
+      (map (fn [n c]
+             (spit (format "bulkinput/em-%d-%d.edn" total chunk)
+               (into [] c) :append (pos? n)))
+        (range)
+        chunks))))
+
+;;; --- specs where dups are likely ---------------------
+
+(def email-usernames
+  #{"tom" "dick" "harry"})
+
+(s/def ::email-address-duped
+  (s/with-gen
+    (s/and string? #(re-matches email-regex %))
+    #(->>
+       (gen/tuple (s/gen email-usernames)
+         (s/gen (set (take 3 email-domains))))
+       (gen/fmap (fn [[addr domain]] (str addr "@" domain))))))
+
+(s/def ::email-record-duped
+  (s/keys :req-un [::email-address-duped ::email-body ::spam-score]))
+
+(defn email-records-duped-test-gen
+  ([] (repeatedly
+        #(gen/generate (s/gen ::email-record-duped))))
+  ([n]
+   (if (nil? n)
+     (email-records-duped-test-gen)
+     (take n (email-records-duped-test-gen)))))
